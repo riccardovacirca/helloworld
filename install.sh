344ea858976a0546734836d2defd830c43ee89ed
riccardovacirca <rvacrca23@gmail.com>
2025-06-03 07:13:05 +0200

    updated

--- install.sh
+++ install.sh
@@ -13,7 +13,9 @@ if [ "$option" = "default" ]; then
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$(pwd):/$SERVICE_NAME" -p $SERVICE_PORT:2310 -p $SERVICE_TLS_PORT:2443 \
     -p $SERVICE_WS_PORT:2791 ubuntu:latest
+  sleep 2
   docker start $SERVICE_NAME
+  sleep 2
   docker exec -i $SERVICE_NAME bash -c "\
     export DEBIAN_FRONTEND=noninteractive && \
     ln -fs /usr/share/zoneinfo/Europe/Rome /etc/localtime && \
@@ -33,8 +35,8 @@ if [ "$option" = "default" ]; then
     apt-get update && apt-get install -y --no-install-recommends \
     nano clang make curl git python3 \
     libssl-dev libsqlite3-dev libapr1-dev libaprutil1-dev libaprutil1-dbd-mysql \
-    libaprutil1-dbd-pgsql libaprutil1-dbd-sqlite3 libjson-c-dev siege \
-    valgrind gdb ca-certificates && apt-get autoremove && apt-get clean \
+    libaprutil1-dbd-pgsql libaprutil1-dbd-sqlite3 libjson-c-dev \
+    docker.io gdb ca-certificates mysql-client \
     && rm -rf /var/lib/apt/lists/*" && echo "Dev packages installed."
   if [ ! -d "microservice" ]; then
     docker exec -i $SERVICE_NAME bash -c "\
@@ -54,28 +56,35 @@ if [ "$option" = "default" ]; then
       && git clone https://github.com/riccardovacirca/mongoose.git \
       && rm -rf mongoose/.git" && echo "Mongoose installed."
   fi
+
   if [ ! -d "unity" ]; then
     docker exec -i $SERVICE_NAME bash -c "\
       cd $SERVICE_NAME \
       && git clone https://github.com/riccardovacirca/Unity.git unity \
       && rm -rf unity/.git" && echo "Unity installed."
   fi
+  echo "Configuring Git..."
   docker exec -i $SERVICE_NAME bash -c "\
-    cd $SERVICE_NAME \
-    && git config --global --add safe.directory $SERVICE_NAME \
+    cd /$SERVICE_NAME \
+    && git config --global --add safe.directory /$SERVICE_NAME \
+    && git config --global --add safe.directory /$SERVICE_NAME/microservice \
+    && git config --global --add safe.directory /$SERVICE_NAME/microtools \
     && git config user.name \"$GITHUB_USER\" \
     && git config user.email \"$GITHUB_MAIL\" \
     && git config user.name && git config user.email" && echo "Git configured."
+  echo "Configuring locale..."
   docker exec -i $SERVICE_NAME bash -c "\
     apt-get install -y language-pack-en; \
     locale-gen en_US.UTF-8; \
     update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8; \
     locale;"
+  echo "Configuring cls command..."
   docker exec -i $SERVICE_NAME bash -c "\
     command -v cls >/dev/null 2>&1 || { \
       echo \"alias cls='clear'\" >> /etc/bash.bashrc \
       && source /etc/bash.bashrc; \
     }"
+  echo "Installation done."
 fi
 if [ "$option" = "mariadb" ]; then
   docker pull mariadb:latest
@@ -84,17 +93,8 @@ if [ "$option" = "mariadb" ]; then
     mariadb:latest
   docker start mariadb
   sleep 3
-  docker exec -i mariadb bash -c "\
-    apt-get update && apt-get install -y --no-install-recommends \
-    mysql-client && apt-get autoremove && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*"
-  docker exec -i mariadb bash -c "\
-    mysql -e \" \
-    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' \
-    IDENTIFIED BY '$DB_ROOT_PASSWORD' WITH GRANT OPTION; \
-    FLUSH PRIVILEGES;\"  2>/dev/null " && echo "Root privileges installed."
-  docker exec -i mariadb bash -c "\
-    mysql -u root -p$DB_ROOT_PASSWORD -e \"
+  docker exec -i $SERVICE_NAME bash -c "\
+    mysql -h mariadb -u root -p$DB_ROOT_PASSWORD -e \"
     CREATE DATABASE IF NOT EXISTS $SERVICE_NAME;
     CREATE USER IF NOT EXISTS '$SERVICE_NAME'@'%' IDENTIFIED BY '$DB_PASSWORD';
     GRANT ALL PRIVILEGES ON $SERVICE_NAME.* \
